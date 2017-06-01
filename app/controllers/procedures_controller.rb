ccb00940c048b23eda9c2ca5a3ff29bee5bb4646
Eleonore Delaire <eleonore.delaire@edu.escpeurope.eu>
2017-06-01 20:10:36 +0200

    trying to improve update

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -1,10 +1,10 @@
 class ProceduresController < ApplicationController
    before_action :set_procedure, only: [:new, :update, :edit, :show]
 
-  def new
+  def show
   end
 
-  def show
+  def new
   end
 
   def create
@@ -31,48 +31,49 @@ class ProceduresController < ApplicationController
     redirect_to root_path
   end
 
-  # def calcul_champs_remplis
-  #   champ_done = 0
-  #   if current_user.procedure.child_nb.nil?
-  #     champ_done =+ 1
-  #   end
-  #   if current_user.procedure.owner_nb.nil?
-  #     champ_done =+ 1
-  #   end
-  #   if current_user.procedure.renter_nb.nil?
-  #     champ_done =+ 1
-  #   end
-  #   if current_user.procedure.status_pro.nil?
-  #     champ_done =+ 1
-  #   end
-  #   if current_user.procedure.bank_account_nb.nil?
-  #     champ_done =+ 1
-  #   end
-  #   if current_user.procedure.credit_nb.nil?
-  #     champ_done =+ 1
-  #   end
-  #   if current_user.procedure.insurance_nb.nil?
-  #     champ_done =+ 1
-  #   end
-  #   if current_user.procedure.vehicle_nb.nil?
-  #     champ_done =+ 1
-  #   end
-  #   if current_user.procedure.contract_type.nil?
-  #     champ_done =+ 1
-  #   end
-  #   if current_user.procedure.status_pro_contact.nil?
-  #     champ_done =+ 1
-  #   end
-  #   if current_user.procedure.marriage_contract.nil?
-  #     champ_done =+ 1
-  #   end
-  #   if current_user.procedure.first_name.nil?
-  #     champ_done =+ 1
-  #   end
-  #   if current_user.procedure.last_name.nil?
-  #     champ_done =+ 1
-  #   end
-  # end
+  def calcul_champs_remplis
+    @champ_done = 0
+    if current_user.procedure.child_nb.nil?
+      champ_done =+ 1
+    end
+    if current_user.procedure.owner_nb.nil?
+      champ_done =+ 1
+    end
+    if current_user.procedure.renter_nb.nil?
+      champ_done =+ 1
+    end
+    if current_user.procedure.status_pro.nil?
+      champ_done =+ 1
+    end
+    if current_user.procedure.bank_account_nb.nil?
+      champ_done =+ 1
+    end
+    if current_user.procedure.credit_nb.nil?
+      champ_done =+ 1
+    end
+    if current_user.procedure.insurance_nb.nil?
+      champ_done =+ 1
+    end
+    if current_user.procedure.vehicle_nb.nil?
+      champ_done =+ 1
+    end
+    if current_user.procedure.contract_type.nil?
+      champ_done =+ 1
+    end
+    if current_user.procedure.status_pro_contact.nil?
+      champ_done =+ 1
+    end
+    if current_user.procedure.marriage_contract.nil?
+      champ_done =+ 1
+    end
+    if current_user.procedure.first_name.nil?
+      champ_done =+ 1
+    end
+    if current_user.procedure.last_name.nil?
+      champ_done =+ 1
+    end
+    return @champ_done
+  end
 
   private
 
@@ -82,42 +83,43 @@ class ProceduresController < ApplicationController
 
   def procedure_params
     params.require(:procedure).permit(:first_name, :last_name, :child_nb, :status_pro, :bank_account_nb, :status_pro_conjoint, :marriage_contract, :passport,
-  :passport_spouse,
-  :passport_children,
-  :identity,
-  :identity_spouse,
-  :identity_children,
-  :acte_naissance,
-  :acte_naissance_spouse,
- :acte_naissance_children,
-  :livret,
-  :acte_mariage,
-  :contract_mariage,
-  :taxe_habitation,
-  :taxe_fonciere,
-  :rent,
-  :bills,
-  :insurance_vehicle,
-  :life_insurance,
-  :insurance_other,
-  :scolarite,
-  :caf,
-  :payroll,
-  :payroll_spouse,
-  :payroll_dec,
-  :payroll_spouse_dec,
-  :bilan_company,
-  :unemployment,
-  :unemployment_spouse,
-  :pro_revenu,
-  :pro_revenu_spouse,
-  :taxes,
-  :taxes_spouse,
-  :property,
-  :revenu_foncier,
-  :bank_account,
-  :carte_grise)
+    :passport_spouse,
+    :passport_children,
+    :identity,
+    :identity_spouse,
+    :identity_children,
+    :acte_naissance,
+    :acte_naissance_spouse,
+   :acte_naissance_children,
+    :livret,
+    :acte_mariage,
+    :contract_mariage,
+    :taxe_habitation,
+    :taxe_fonciere,
+    :rent,
+    :bills,
+    :insurance_vehicle,
+    :life_insurance,
+    :insurance_other,
+    :scolarite,
+    :caf,
+    :payroll,
+    :payroll_spouse,
+    :payroll_dec,
+    :payroll_spouse_dec,
+    :bilan_company,
+    :unemployment,
+    :unemployment_spouse,
+    :pro_revenu,
+    :pro_revenu_spouse,
+    :taxes,
+    :taxes_spouse,
+    :property,
+    :revenu_foncier,
+    :bank_account,
+    :carte_grise)
   end
+
 end
 
 
