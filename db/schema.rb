8e1b937d5d6ea93e00cd123224528fdd31881a82
Kheira Benmeridja <kheira.benmeridja@gmail.com>
2017-06-02 18:01:59 +0200

    avocats seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601175403) do
+ActiveRecord::Schema.define(version: 20170602152723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20170601175403) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "phone_number"
+    t.string "email"
   end
 
   create_table "procedures", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,14 @@ Avocat.destroy_all
 puts 'On récupere les adresses des avocats'
 
 store = []
-for i in (1..5) do
+for i in (0...5) do
 html_doc = Nokogiri::HTML(open("http://www.avocatsparis.org/Eannuaire/CMSListeRecherche.aspx?nom=&Pre=&ChReNom=2&Adr=&Arr=-1&mail=False&Site=False&Toque=&etranger=False&Spec=25&page=#{i}"), nil, 'UTF-8')
 html_doc.search('.corps3newannuaire\"').each do |info|
   avocat = {}
   avocat[:name] = info.search('.lien3').first.search('strong').text
   avocat[:address] = info.search('.newannuair10px')[2].text
+  avocat[:phone_number] = info.search('.lien3')[3].text
+  avocat[:email] = info.search('.lien3 a').last.text
   store << avocat
   end
 end
