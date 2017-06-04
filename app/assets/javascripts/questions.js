5570bfeaa824a68516c952a043028e9e49157776
claire schults <claire.schults@gmail.com>
2017-06-04 19:08:42 +0200

    structure in place

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
-  $('.select-item').on('click', function() {
-    $('.active').removeClass('active');
-    $(this).addClass('active');
-    var value = $(this).text();
-    $(this).parent().prev().children('input').val(value);
-  })
+  if ($('#questions').length > 0) {
+    $('.answer').on('click', function() {
+      $('.active').removeClass('active');
+      $(this).addClass('active');
+      var value = $(this).data('input');
+      $(this).parent().prev().children('input').val(value);
+    });
+
+    $('.next').on('click', function() {
+      $(this).parent().slideUp();
+    })
+    $('.prev').on('click', function() {
+      $(this).parent().prev().slideDown().css('display', 'flex');
+    })
+  }
 })
