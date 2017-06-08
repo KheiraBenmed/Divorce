95f8d54d455992f382ce8c8dfacfc564185aec8e
Eleonore Delaire <eleonore.delaire@edu.escpeurope.eu>
2017-06-08 12:19:07 +0200

    front enfin satisfaisant

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -160,135 +160,111 @@ class ProceduresController < ApplicationController
 
   def calcul_doc_non_uploadés
     @doc_not_upload = 0
-    if current_user.procedure[:passport].nil?
+    if !current_user.procedure[:passport].nil?
       @doc_not_upload += 1
     end
-    if current_user.procedure[:passport_spouse].nil?
+    if !current_user.procedure[:passport_spouse].nil?
       @doc_not_upload += 1
     end
-    if current_user.procedure[:passport_children].nil?
+    if !current_user.procedure[:passport_children].nil?
       @doc_not_upload += 1
     end
-    if current_user.procedure[:identity].nil?
+    if !current_user.procedure[:identity].nil?
       @doc_not_upload += 1
     end
-    if current_user.procedure[:identity_spouse].nil?
+    if !current_user.procedure[:identity_spouse].nil?
       @doc_not_upload += 1
     end
-    if current_user.procedure[:identity_children].nil?
+    if !current_user.procedure[:identity_children].nil?
       @doc_not_upload += 1
     end
-    if current_user.procedure[:acte_naissance].nil?
+    if !current_user.procedure[:acte_naissance].nil?
       @doc_not_upload += 1
     end
-    if current_user.procedure[:acte_naissance_spouse].nil?
+    if !current_user.procedure[:acte_naissance_spouse].nil?
       @doc_not_upload += 1
     end
-    if current_user.procedure[:acte_naissance_children].nil?
+    if !current_user.procedure[:acte_naissance_children].nil?
       @doc_not_upload += 1
     end
-    if current_user.procedure[:livret].nil?
+    if !current_user.procedure[:livret].nil?
       @doc_not_upload += 1
     end
-    if current_user.procedure[:acte_mariage].nil?
+    if !current_user.procedure[:acte_mariage].nil?
       @doc_not_upload += 1
     end
-    if current_user.procedure[:contract_mariage].nil?
+    if !current_user.procedure[:contract_mariage].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:taxe_habitation].nil?
+    if !current_user.procedure[:taxe_habitation].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:taxe_fonciere].nil?
+    if !current_user.procedure[:taxe_fonciere].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:rent].nil?
+    if !current_user.procedure[:rent].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:bills].nil?
+    if !current_user.procedure[:bills].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:insurance_vehicle].nil?
+    if !current_user.procedure[:insurance_vehicle].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:insurance_other].nil?
+    if !current_user.procedure[:insurance_other].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:scolarite].nil?
+    if !current_user.procedure[:scolarite].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:caf].nil?
+    if !current_user.procedure[:caf].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:payroll].nil?
+    if !current_user.procedure[:payroll].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:payroll_spouse].nil?
+    if !current_user.procedure[:payroll_spouse].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:payroll_dec].nil?
+    if !current_user.procedure[:payroll_dec].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:payroll_spouse_dec].nil?
+    if !current_user.procedure[:payroll_spouse_dec].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:bilan_company].nil?
+    if !current_user.procedure[:bilan_company].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:unemployment].nil?
+    if !current_user.procedure[:unemployment].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:unemployment_spouse].nil?
+    if !current_user.procedure[:unemployment_spouse].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:pro_revenu].nil?
+    if !current_user.procedure[:pro_revenu].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:pro_revenu_spouse].nil?
+    if !current_user.procedure[:pro_revenu_spouse].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:taxes].nil?
+    if !current_user.procedure[:taxes].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:taxes_spouse].nil?
+    if !current_user.procedure[:taxes_spouse].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:property].nil?
+    if !current_user.procedure[:property].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:revenu_foncier].nil?
+    if !current_user.procedure[:revenu_foncier].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:bank_account].nil?
+    if !current_user.procedure[:bank_account].nil?
       @doc_not_upload += 1
     end
-
-    if current_user.procedure[:carte_grise].nil?
+    if !current_user.procedure[:carte_grise].nil?
       @doc_not_upload += 1
     end
-
     return @doc_not_upload
   end
 
